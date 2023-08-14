3cd3e37a7a8ee92082b5941f7db50c4d404606ea
seongminn <berry9901@gmail.com>
2023-08-15 02:34:37 +0900

    LV.1 비밀 지도

---
+++ programmers.js/LV.1_비밀 지도.js
@@ -0,0 +1,9 @@
+function solution(n, arr1, arr2) {
+  return arr1.map((nums, idx) =>
+    (nums | arr2[idx])
+      .toString(2)
+      .padStart(n, '0')
+      .replace(/1/g, '#')
+      .replace(/0/g, ' ')
+  );
+}
