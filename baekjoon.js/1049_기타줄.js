180ad8d91f23fd0791a0e66203900a2d7ff0d588
seongminn <berry9901@gmail.com>
2023-08-19 15:57:58 +0900

    1049. 기타줄

---
+++ baekjoon.js/1049_기타줄.js
@@ -0,0 +1,18 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n');
+
+const [N, M] = input.shift().split(' ').map(Number);
+const package = Math.min(...input.map((str) => str.split(' ').map(Number)[0]));
+const each = Math.min(...input.map((str) => str.split(' ').map(Number)[1]));
+
+console.log(
+  Math.min(
+    N * each,
+    Math.ceil(N / 6) * package,
+    Math.floor(N / 6) * package + (N % 6) * each
+  )
+);
