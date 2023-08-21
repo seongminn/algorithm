e068589ad4c189bdbdfe5e42079571f387ad76ab
seongminn <berry9901@gmail.com>
2023-08-21 23:53:17 +0900

    1049. 기타줄

--- baekjoon.js/1049_기타줄.js
+++ baekjoon.js/1049_기타줄.js
@@ -1,3 +1,14 @@
+/**
+ * 1049. 기타줄
+ *
+ * 작성자: seongminn
+ * 난이도: Silver 4
+ * 언어: node.js
+ * 사용 메모리: 9,348 KB
+ * 소요 시간: 124 ms
+ * 해결 날짜: 2023년 8월 19일
+ */
+
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const input = require('fs')
   .readFileSync(filePath)
